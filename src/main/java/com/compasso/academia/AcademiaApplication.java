497fc7fd7a26463d71678ad220759c2917843a1d
Joao Junqueira <joao.nascimento_bols@compasso.com.br>
2021-04-19 22:50:16 -0700

    project init

---
+++ src/main/java/com/compasso/academia/AcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.compasso.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademiaApplication.class, args);
+    }
+
+}
