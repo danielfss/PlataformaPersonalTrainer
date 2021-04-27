c9de6383fedd074f7832cad848ea266cd1dcc950
AlexSSantos <alex.santos_BOLS@compasso.com.br>
2021-04-27 19:44:27 -0300

    Add dependencias e classe de testes

--- src/test/java/com/compasso/academia/AcademiaApplicationTests.java
+++ src/test/java/com/compasso/academia/AcademiaApplicationTests.java
@@ -1,13 +1,52 @@
 package com.compasso.academia;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.compasso.academia.model.Role;
+import com.compasso.academia.model.Usuario;
+import com.compasso.academia.repository.RoleRepository;
+import com.compasso.academia.repository.UsuarioRepository;
+
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 class AcademiaApplicationTests {
 
+	@Autowired 
+	RoleRepository roleRepo;
+	
+	@Autowired 
+	UsuarioRepository repoUs;
+	
+	
     @Test
     void contextLoads() {
     }
 
+
+
+@Test
+public void testAddRoleToNewUser() { 
+	Usuario usuario = new Usuario();
+	usuario.setNome("alex33");
+	usuario.setEmail("joao@email.com");
+	usuario.setSenha("87654321");
+	usuario.setTelefone("719000000");
+	usuario.setEnabled(true);
+  
+	Role role = roleRepo.findByName("PERSONAL"); 
+  
+	usuario.addRole(role);
+  
+	repoUs.save(usuario);
+
+	}
 }
