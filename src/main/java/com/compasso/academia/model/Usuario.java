f849c79b2b5fe241345c534c30178127e2a63cc9
Rafael Ribeiro <rafael.ribeiro_BOLS@compasso.com.br>
2021-04-27 15:26:43 -0300

    Excluindo a tabela de Perfil que será substituida por Role. Gitignore não mais vendo o application.properties

--- src/main/java/com/compasso/academia/model/Usuario.java
+++ src/main/java/com/compasso/academia/model/Usuario.java
@@ -1,32 +1,27 @@
 package com.compasso.academia.model;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
-import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Entity
 @Table(name = "Usuarios")
-public class Usuario implements UserDetails
+public class Usuario
 {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String nome;
     private String email;
     private String senha;
-    private boolean enabled = true;
-    private boolean debtor = true;
-
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    private List<Perfil> perfis = new ArrayList<>();
+    private Boolean devedor;
 
     public Long getId() {
         return id;
@@ -34,14 +29,6 @@ public class Usuario implements UserDetails
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String usuario) {
-        this.nome = usuario;
     }
 
     public String getSenha() {
@@ -61,38 +48,11 @@ public class Usuario implements UserDetails
         this.email = email;
     }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.perfis;
+    public Boolean getDevedor() {
+        return devedor;
     }
 
-    @Override
-    public String getPassword() {
-        return this.senha;
-    }
-
-    @Override
-    public String getUsername() {
-        return this.email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return this.enabled;
+    public void setDevedor(Boolean devedor) {
+        this.devedor = devedor;
     }
 }
