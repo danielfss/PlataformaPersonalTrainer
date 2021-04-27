eb1296590d84f13304d5d11c3414874d37cf0b13
AlexSSantos <alex.santos_BOLS@compasso.com.br>
2021-04-27 12:47:22 -0300

    Anotações nos repositórios

--- src/main/java/com/compasso/academia/repository/UsuarioRepository.java
+++ src/main/java/com/compasso/academia/repository/UsuarioRepository.java
@@ -1,13 +1,12 @@
 package com.compasso.academia.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import com.compasso.academia.model.Treino;
 import com.compasso.academia.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
 	@Query("SELECT u FROM Usuario u WHERE u.email = ?1")
