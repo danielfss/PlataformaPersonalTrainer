603ca65230cf607852e1630c88e4904fb23e7424
AlexSSantos <alex.santos_BOLS@compasso.com.br>
2021-04-29 11:39:10 -0300

    alterações em agenda/layout/treino

--- src/main/java/com/compasso/academia/controller/AgendaController.java
+++ src/main/java/com/compasso/academia/controller/AgendaController.java
@@ -1,5 +1,6 @@
 package com.compasso.academia.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,7 +28,7 @@ import com.compasso.academia.service.AppService;
 
 
 @Controller
-public class AulaController {
+public class AgendaController {
 
 	@Autowired
 	private AppService service;
@@ -38,38 +40,23 @@ public class AulaController {
 	private UsuarioRepository usuarioRepo;
 	
 	@RequestMapping(value = "/dashboard/agenda_aluno", method=RequestMethod.GET)
-	public String viewDashboardAluno(Model model, Model modelPegaIdHorario ) {
-		List<Aula> aulas = aulaRepository.findAll();
+	public String viewAgendaAluno(Model model, Model modelPegaIdHorario) {
+		List<Aula> aula = aulaRepository.findAll();
 		
-		model.addAttribute("aulas", aulas);
+		List<Aula> disponivel = new ArrayList<>();		
+		
+		 for(Aula tp : aula){    
+			   if(tp.getAluno() == null) {
+				   disponivel.add(tp);				   
+			   }
+			 }
+		model.addAttribute("disponivel", disponivel);
 		
 		modelPegaIdHorario.addAttribute("identificador", new Identificador());
 		
 		return "/dashboard/agenda_aluno";
 	}
-
-	@GetMapping("/personal/agenda_personal")
-	public String viewAgendaPersonal(AulaDto aulaDto, Model model) {
-		List<Aula> aulas = service.getAulas();
-		model.addAttribute("aulas", aulas);
-		return "/personal/agenda_personal";
-	}
-
-	/* Endpoint do professor */
-	@PostMapping("inserirAula")
-	public String inserirAula(@Valid AulaDto aulaDto, BindingResult result) {
-
-		if (result.hasErrors()) {
-			return "/personal/agenda_personal";
-		}
-
-		Aula aula = aulaDto.toAula();
-		aulaRepository.save(aula);
-
-		return "redirect:/personal/agenda_personal";
-	}
-
-	/* Endpoint do aluno */
+	
 	@RequestMapping(value = "/dashboard/agenda_aluno", method=RequestMethod.POST)
 	public String AgendaAluno(@AuthenticationPrincipal UsuarioDetails loggedUser, @ModelAttribute Identificador ident, Model model){
 		
@@ -84,7 +71,60 @@ public class AulaController {
 		
 		aulaRepository.save(agendada);
 		
-		return "redirect:/dashboard/agenda_aluno";
+		return "redirec:/dashboard/agenda_aluno";
 	}
-	/* FIM CONTROLLER AULA */
+	
+	@GetMapping("/personal/agenda_personal")
+	public String viewAgendaPersonal(AulaDto aulaDto, Model model, Model modelAgendados) {
+		List<Aula> aulas = service.getAulas();
+		
+		List<Aula> agendados = new ArrayList<>();
+		for(Aula tp : aulas){    
+			   if(tp.getAluno() != null) {
+				   agendados.add(tp);
+			   }
+			 }
+	
+		model.addAttribute("aulas", aulas);
+		
+		modelAgendados.addAttribute("agendados", agendados);
+		
+		return "/personal/agenda_personal";
+	}
+
+	@RequestMapping(value = "/deletaHorario/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
+	public String deletaHorario(@PathVariable (value ="id") Long id) {
+		
+		service.deleteAgendado(id);
+		
+		return "redirect:/personal/agenda_personal";
+		
+	}
+	
+	@RequestMapping(value = "/deletaAgendado/{id}", method = {RequestMethod.DELETE, RequestMethod.GET})
+	public String deletaAgendado(@PathVariable (value ="id") Long id) {
+		
+		service.deleteAgendado(id);
+		
+		return "redirect:/personal/agenda_personal";
+		
+	}
+	
+	
+	
+	/* Endpoint do professor */
+	@PostMapping("inserirAula")
+	public String inserirAula(@Valid AulaDto aulaDto, BindingResult result) {
+
+		if (result.hasErrors()) {
+			return "/personal/agenda_personal";
+		}
+
+		Aula aula = aulaDto.toAula();
+		aulaRepository.save(aula);
+
+		return "redirect:/personal/agenda_personal";
+	}
+
+	
 }
